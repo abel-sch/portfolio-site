26711c5824f2f0814ffe25c49355cc4f5b09a1b8
Abel <abel_schupp@hotmail.com>
2023-01-09 21:56:28 +0100

    tweak hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,36 @@
+import {motion} from 'framer-motion';
+
 export default function Hero(props) {
+
+	const variants = {
+		initial: {
+			opacity: 0
+		},
+		animate: {
+			opacity: 1,
+			transition: {
+				delay: .5 ,
+				duration: .5
+			}
+		},
+		exit: {
+			opacity: 1
+		}
+	}
+
 	return (
 		<header className="w-full h-screen flex flex-col">
 			<div className="grow"></div>
 			<div className="font-bold text-6xl py-4 px-4">
-				<h1>
+				<motion.h1
+					initial="initial"
+					animate="animate"
+					exit="exit"
+					variants={variants}
+				>
 					Abel Schupp <br/>
 					<span className="font-normal">Designer</span> + <span className="font-normal">Developer</span>
-				</h1>
+				</motion.h1>
 				<p className="font-medium text-white">hallo@abelschupp.nl</p>
 			</div>
 		</header>
