68bee3df5a9bc0f72644a6ed09106111bf436496
Abel <abel_schupp@hotmail.com>
2023-03-15 18:40:04 +0100

    Update Mockup.tsx

--- components/content/Mockup.tsx
+++ components/content/Mockup.tsx
@@ -9,7 +9,7 @@ export default function Mockup(props: Props) {
     } = props;
 
     return (
-        <section className="w-full bg-grey py-8 lg:py-24 flex justify-center">
+        <section className="w-full bg-grey py-8 lg:py-24 flex justify-center mb-12 lg:mb-24">
             { video && (
                 <video className="max-w-screen-sm w-full" autoPlay muted loop playsInline>
                     <source src={video} type="video/mp4"/>
