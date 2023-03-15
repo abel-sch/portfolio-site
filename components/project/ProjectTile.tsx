35913de046fd4808918a72f54ee30ea25749d384
Abel <abel_schupp@hotmail.com>
2023-03-15 22:00:44 +0100

    add smoothscroller

--- components/project/ProjectTile.tsx
+++ components/project/ProjectTile.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import {useRef} from 'react';
 import smoothScroller from '@scripts/utils/SmoothScroller';
 import useIsomorphicLayoutEffect from '@scripts/hooks/useIsomorphicLayoutEffect';
-
+import useResizeObserver from '@scripts/hooks/useResizeObserver';
 
 type project = {
     title: string,
@@ -21,11 +21,22 @@ export default function ProjectTile(props: Props) {
     } = props;
 
     const ref = useRef(null);
+    const containerRef = useRef(null);
+    const containerSize = useResizeObserver(containerRef);
+    const textSize = useResizeObserver(ref);
+
+    console.log(textSize);
+
+    // console.log(containerSize);
 
 	useIsomorphicLayoutEffect(() => {
-		if (ref.current) {
+		if (ref.current && containerRef.current) {
             smoothScroller.add(ref.current, o => {
-                console.log(o);
+                if (containerSize) {
+                    const maxOffset = textSize.width - containerSize.width;
+                    console.log(maxOffset, textSize.width, containerSize.width);
+                    ref.current.style.transform = `translate3d(-${o.factor * maxOffset}px, 0, 0)`;
+                }
             });
 		}
 
@@ -34,19 +45,20 @@ export default function ProjectTile(props: Props) {
 				smoothScroller.remove(ref.current);
 			}
 		};
-	}, [ref]);
+	}, [ref, containerSize, textSize]);
 
     return (
         <Link
             href={project.slug}
             className={`
-                bg-white text-8xl lg:text-[10rem] font-bold px-4 py-16 lg:p-8 block group
+                bg-white text-8xl lg:text-[10rem] font-bold px-4 py-16 lg:p-8 flex group
                 hover:text-white hover:bg-black
                 transition ease-in-out duration-700
                 relative overflow-hidden
             `}
         >
-        <span ref={ref}>{project.title}</span>
+        <div className='block' ref={ref}>{project.title}</div>
+        <div ref={containerRef} className='absolute top-0 bottom-0 left-4 right-4 lg:left-8 right-8'></div>
         <Image
             src={project.thumbnail}
             className={`
