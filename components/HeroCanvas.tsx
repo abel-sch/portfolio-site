5edab9097488d00f772170e8da20c9765f0a4c51
Abel <abel_schupp@hotmail.com>
2023-03-15 16:18:36 +0100

    add animation

--- components/HeroCanvas.tsx
+++ components/HeroCanvas.tsx
@@ -1,30 +1,34 @@
 import Sketch from "react-p5";
 import p5Types from "p5";
-import {useEffect, useRef} from "react";
+import {animate as animateMotion} from 'framer-motion';
+import {useEffect, useRef, useState} from "react";
 
 interface ComponentProps {
 	rect: DOMRectReadOnly
 }
 
 const OFFSET = 60.0 as const;
-let isAnimating = false;
 let roofWidth = .5;
 let stickHeight = .5;
+let progress = 0;
 
 export default function HeroCanvas(props : ComponentProps) {
-	const sketchInstance = useRef<p5Types | null>(null);
-	
 	const {
 		rect: {
 			height,
 			width
 		}
 	} = props;
-	
-	useEffect(() => {
-		isAnimating = true;
-	}, [width, height]);
 
+	const sketchInstance = useRef<p5Types | null>(null);
+	const [prevValues, setPrevValues] = useState<{roofWidth: number, stickHeight: number}>({roofWidth, stickHeight});
+	const [nextValues, setNextValues] = useState<{roofWidth: number, stickHeight: number}>({roofWidth: 0, stickHeight: 0});
+	const [isAnimating, setIsAnimating] = useState<boolean>(false);
+	const [isResizing, setIsResizing] = useState<boolean>(false);
+
+	// const [progress, setProgress] = useState<number>(0);
+		
+	const ease = (t: number): number  => t * (2 - t);
 	const drawLetter = (p5: p5Types) => {
 		// Roof
 		const maxHalfRoofWidth = (.5 * width) - OFFSET;
@@ -54,35 +58,73 @@ export default function HeroCanvas(props : ComponentProps) {
 		p5.endShape();
 	}
 
-	const onMousePressed = () => {
+	const onMousePressed = async () => {
 		if (sketchInstance.current) {
-			roofWidth = Math.random();
-			stickHeight = Math.random();
-	
-			drawLetter(sketchInstance.current);
+			startAnimation();
 		}
 	}
 	
 	const setup = (p5: p5Types, canvasParentRef: Element) => {
 		p5.createCanvas(width, height).parent(canvasParentRef);
 		sketchInstance.current = p5;
-
 		drawLetter(p5);
 	};
 
 	const draw = (p5: p5Types) => {
+		if (isResizing) {
+			p5.resizeCanvas(width, height);
+			drawLetter(p5);
+			setIsResizing(false);
+		}
 		if (isAnimating) {
-			animate(p5)
+			animate(p5);
 		}
 	};
 
-	const animate = (p5: p5Types) => {
-		p5.resizeCanvas(width, height);
+	const startAnimation = () => {
+		const targetRoofWidth = Math.random();
+		const targetStickHeight = Math.random();
 
-		drawLetter(p5);
+		setNextValues({
+			roofWidth: targetRoofWidth,
+			stickHeight: targetStickHeight
+		});
 
-		isAnimating = false;
+		setPrevValues({
+			roofWidth,
+			stickHeight
+		});
+
+		progress = 0;
+		setIsAnimating(true);
 	}
+
+	const stopAnimation = () => {
+		setIsAnimating(false);
+	}
+
+	const animate = (p5: p5Types) => {
+		if (progress >= 1) {
+			stopAnimation();
+		} 
+		// setProgress(progress => progress + .05);
+		progress += .05;
+		roofWidth = prevValues.roofWidth + (nextValues.roofWidth - prevValues.roofWidth) * ease(progress);
+		stickHeight = prevValues.stickHeight + (nextValues.stickHeight - prevValues.stickHeight) * ease(progress);
+		drawLetter(p5);
+	}
+
+	useEffect(() => {
+		setIsResizing(true);
+	}, [width, height]);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			startAnimation();
+		}, 1000);
+
+		return () => clearInterval(timer);
+	}, []);
 
 	return (
 		<div className="absolute inset-0" onClick={onMousePressed}>
