5a979c3eee3018d0dca963e2eccb5f460601322d
Abel <abel_schupp@hotmail.com>
2023-03-15 18:04:30 +0100

    add project description

--- pages/design/[slug].js
+++ pages/design/[slug].js
@@ -1,14 +1,17 @@
 import Head from 'next/head';
 import getSlugs from '@scripts/content/getSlugs';
 import getContent from '@scripts/content/getContent';
-import ProjectHeader from '@components/ProjectHeader';
+import ProjectHeader from '@components/project/ProjectHeader';
+import ProjectDescription from '@components/project/ProjectDescription';
 
 export default function Page(props) {
 	const {
 		attributes: {
 			title = '',
             featured_image = ''
-		} = {}
+		} = {},
+        intro,
+        html
 	} = props;
 
     console.log(props);
@@ -19,7 +22,7 @@ export default function Page(props) {
 			</Head>
 			<article className="h-full w-full bg-black grow">
                 <ProjectHeader featured_image={featured_image}/>
-                {/* <ProjectDescription/> */}
+                <ProjectDescription intro={intro} content={html} title={title}/>
 			</article>
 		</>
 	)
