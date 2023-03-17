f65286a07b1edeb94e3ff1129471b28d345b2b65
Abel Schupp <abel_schupp@hotmail.com>
2023-03-17 14:51:54 +0100

    prevent bg text wrap

--- components/project/ProjectDescription.tsx
+++ components/project/ProjectDescription.tsx
@@ -13,7 +13,7 @@ export default function ProjectDescription(props: Props) {
 
     return (
         <div className="text-white p-4 pt-16 lg:p-24 lg:py-28 mx-auto relative overflow-hidden">
-            { title && <h1 className="absolute top-0 left-0 text-[10rem] lg:text-[15rem] font-bold -translate-y-1/2 opacity-5">{title}</h1> }
+            { title && <h1 className="absolute top-0 left-0 text-[10rem] lg:text-[15rem] font-bold -translate-y-1/2 opacity-5 whitespace-nowrap">{title}</h1> }
             <div className="mx-auto flex max-w-screen-lg">
                 <div className="prose prose-invert">
                     { intro && <div className="text-2xl lg:text-4xl lg:leading-snug leading-normal" dangerouslySetInnerHTML={{__html: intro}}></div> }
