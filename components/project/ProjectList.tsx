1e3a5656056081488577e58d13f81d1deddd624b
Abel Schupp <abel_schupp@hotmail.com>
2023-03-17 15:48:14 +0100

    use tabs

--- components/project/ProjectList.tsx
+++ components/project/ProjectList.tsx
@@ -2,27 +2,27 @@ import {useState} from 'react';
 import ProjectTile from "./ProjectTile";
 
 type project = {
-    title: string,
-    slug: string,
-    thumbnail: string
+	title: string,
+	slug: string,
+	thumbnail: string
 }
 
 interface Props {
-    projects: project[]
+	projects: project[]
 }
 
 export default function ProjectList(props: Props) {
-    const {
-        projects
-    } = props;
+	const {
+		projects
+	} = props;
 
-    return (
-        <ul className='mt-24'>
-        { projects.map(project => (
-            <li key={project.slug}>
-                <ProjectTile project={project}/>
-            </li>
-        ))}
-        </ul>
-    );
+	return (
+		<ul className='mt-24'>
+			{ projects.map(project => (
+				<li key={project.slug}>
+					<ProjectTile project={project}/>
+				</li>
+			))}
+		</ul>
+	);
 }
