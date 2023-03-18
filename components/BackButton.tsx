6fb9ddddec8181b9e85962abb5536a9973a2f821
Abel <abel_schupp@hotmail.com>
2023-03-18 11:41:10 +0100

    add back link

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,9 +1,12 @@
 import {useState} from "react";
 import {motion, useScroll, useMotionValueEvent, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function BackButton() {
 	const { scrollY } = useScroll();
 	const [isVisible, setIsVisible] = useState<boolean>(false);
+	const router = useRouter();
 
 	useMotionValueEvent(scrollY, "change", (latest) => {
 		if (latest >= 100 && !isVisible) {
@@ -13,6 +16,14 @@ export default function BackButton() {
 		}
 	  });
 
+	if (router.asPath == '/') return;
+
+	const backPath = (path: string) : string => {
+		const dirs = path.split('/');
+		const lastDir = dirs[dirs.length - 1].length;
+		return path.substring(0, path.length - lastDir);
+	}
+
 	return (
 		<motion.div className="fixed top-4 left-4 z-50 text-white drop-shadow-xl text-5xl lg:text-8xl font-bold">
 			<AnimatePresence>
@@ -21,7 +32,8 @@ export default function BackButton() {
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						exit={{ opacity: 0 }}
-					>←</motion.div>
+					><Link href={backPath(router.asPath)}>←</Link>
+					</motion.div>
 				)}	
 			</AnimatePresence>
 		</motion.div>
--- scripts/utils/getLinkTarget.ts
+++ scripts/utils/getLinkTarget.ts
@@ -1,3 +1,3 @@
-export const getLinkTarget = (href: string): string | false => {
-	return href.startsWith('/') ? false : '_blank';
+export const getLinkTarget = (href: string): string | undefined => {
+	return href.startsWith('/') ? undefined : '_blank';
 }
