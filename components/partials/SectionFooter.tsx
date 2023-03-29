a93c75c07f32d7a269434b94af52f5ef0a529d21
Abel Schupp <abel_schupp@hotmail.com>
2023-03-29 13:52:50 +0200

    add hover animation to line

--- components/partials/SectionFooter.tsx
+++ components/partials/SectionFooter.tsx
@@ -1,6 +1,8 @@
 import Container from "@components/Container";
 import Link from "next/link";
-import { Fragment } from "react";
+import { Fragment, useRef } from "react";
+import {motion} from "framer-motion";
+import useResizeObserver from "@scripts/hooks/useResizeObserver";
 
 interface Props {
 	pages: Page[],
@@ -46,5 +48,26 @@ function SectionLink(props: LinkProps) {
 		children
 	} = props;
 
-	return <Link href={href} className="font-bold bg-gradient-to-r from-blue to-blue bg-left-bottom bg-no-repeat bg-[length:100%_0.15em]">{children}</Link>
+	const ref = useRef(null);
+	const size = useResizeObserver(ref);
+	const width = size ? size.width * -1 : 0;
+
+	return (
+		<Link href={href} >
+			<motion.div
+				ref={ref}
+				whileHover={{
+					backgroundPositionX: '0px',
+					transition: {
+						ease: 'easeInOut',
+						duration: .5
+					}
+				}}
+				style={{backgroundPositionX: `${-1 * size.width}px`}}
+				className="inline-block font-bold bg-gradient-to-r from-blue to-blue bg-left-bottom bg-no-repeat bg-[length:100%_0.15em]"
+			>
+				{children}
+			</motion.div>
+		</Link>
+	)
 }
