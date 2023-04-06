89e58cd7362607dc83607ef6b385b4886b328b14
Abel Schupp <abel_schupp@hotmail.com>
2023-04-06 18:10:27 +0200

    remove old smooth scroller

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import '../styles/globals.css';
 import { Manrope } from '@next/font/google';
 import { useRouter } from 'next/router';
-import {useEffect, useRef} from 'react';
+import { useEffect } from 'react';
 import { AnimatePresence,motion } from 'framer-motion';
 import PageWrapper from '@components/partials/PageWrapper';
-import SmoothScrollContainer from '@components/SmoothScrollContainer';
 
 
 const inter = Manrope({
@@ -15,7 +14,6 @@ const inter = Manrope({
 export default function MyApp({ Component, pageProps }) {
 	const router = useRouter();
 	const isHome = router.asPath == '/';
-	const smoothScrollContainerRef = useRef();
 	// const variants = {
 	// 	initial: {
 	// 		y: ["0%", "0"],
@@ -47,12 +45,12 @@ export default function MyApp({ Component, pageProps }) {
 	}, [router])
 
 	return (
-		<SmoothScrollContainer ref={smoothScrollContainerRef} scrollFactor={0}>
+		<>
 			{/* <AnimatePresence initial={false} mode="wait" onExitComplete={() => window.scrollTo(0, 0)}> */}
 				<PageWrapper key={router.asPath} className={`${inter.variable} font-sans`}>
 					<Component {...pageProps}/>
 				</PageWrapper>
 			{/* </AnimatePresence> */}
-		</SmoothScrollContainer>
+		</>
 	)
 }
